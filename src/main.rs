c0b3f3bddf21c5bf933fb2fa7ace73b246752d2c
pmrcunha <paulomrcunha@gmail.com>
2022-07-17 20:24:33 +0100

    add more tests

--- src/main.rs
+++ src/main.rs
@@ -160,14 +160,74 @@ fn main() {
     }
 }
 
-#[test]
-fn test_csv_processing() {
-    let output = process_csv(&Path::new("sample_files/transactions.csv")).unwrap();
-    let expected = String::from(
-        r"client, available, held, total, locked
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+    use crate::process_csv;
+
+    fn test_csv(file_path: &str, expected: &str) {
+        let output = process_csv(&Path::new(file_path)).unwrap();
+        let expected_output = String::from(expected);
+        assert_eq!(output, expected_output);
+    }
+
+
+    #[test]
+    fn test_multiple_clients() {
+        test_csv("sample_files/multiple_clients.csv", r"client, available, held, total, locked
 1, 2.0000, 0.0000, 2.0000, true
 2, 0.5000, 0.0000, 0.5000, false
-3, 0.0000, 5.5000, 5.5000, false"
-    );
-    assert_eq!(output, expected);
+3, 0.0000, 5.5000, 5.5000, false");
+    }
+
+    #[test]
+    fn test_deposit_withdrawal() {
+        test_csv("sample_files/deposit_withdrawal.csv", r"client, available, held, total, locked
+1, 1.5000, 0.0000, 1.5000, false
+2, 0.5000, 0.0000, 0.5000, false");
+    }
+
+    #[test]
+    fn test_missing_transactions() {
+        let expected = r"client, available, held, total, locked
+1, 1.0000, 0.0000, 1.0000, false";
+
+        test_csv("sample_files/dispute_missing_transaction.csv", expected);
+        test_csv("sample_files/resolve_missing_transaction.csv", expected);
+        test_csv("sample_files/chargeback_missing_transaction.csv", expected);
+    }
+
+    #[test]
+    fn test_dispute_chargeback() {
+        let expected = r"client, available, held, total, locked
+1, 0.0000, 0.0000, 0.0000, true";
+        test_csv("sample_files/dispute_chargeback.csv", expected);
+    }
+
+    #[test]
+    fn test_dispute_resolve() {
+        let expected = r"client, available, held, total, locked
+1, 1.0000, 0.0000, 1.0000, false";
+        test_csv("sample_files/dispute_resolve.csv", expected);
+    }
+
+    #[test]
+    fn test_dispute() {
+        let expected = r"client, available, held, total, locked
+1, 0.0000, 1.0000, 1.0000, false";
+        test_csv("sample_files/dispute.csv", expected);
+    }
+
+    #[test]
+    fn test_invalid_csv() {
+        assert!(process_csv(&Path::new("sample_files/invalid_csv.csv")).is_err());
+    }
+
+    #[test]
+    fn test_withdrawal_insufficient_funds() {
+        let expected = r"client, available, held, total, locked
+1, 1.0000, 0.0000, 1.0000, false";
+        test_csv("sample_files/withdrawal_insufficient_funds.csv", expected);
+    }
+    
 }
